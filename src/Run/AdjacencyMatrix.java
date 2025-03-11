dec62b1f5f8d9504c6ad00d4d97bf871ffe33ab5
Levia-del <levlola579@gmail.com>
2025-03-10 18:37:50 -0700

    3/10 Adjacency Matrix Creation

---
+++ src/Run/AdjacencyMatrix.java
@@ -0,0 +1,13 @@
+package Run;
+
+public class AdjacencyMatrix {
+
+	private String[] nodes;
+	private int[][] links;
+	
+	public AdjacencyMatrix(String[] nodes, int[][] links)
+	{
+		this.nodes = nodes;
+		this.links = links;
+	}
+}
--- src/Run/main.java
+++ src/Run/main.java
@@ -3,20 +3,6 @@ package Run;
 public class main {
 
 	public static void main(String[] args) {
-		String print = "";
-		AVLTree my = new AVLTree();
-		for(int i = 0;i<10;i++)
-		{
-			int add = (int)((Math.random()*8)+1);
-		    my.add(add);
-		    print += add;
-		    
-		    //System.out.println(my);
-		    //System.out.println("Next Step");
-		}
-		System.out.println(print);
-		System.out.println(my);
-		//my.search((int)((Math.random()*10)+1));
-	}
-
+		
+}
 }
